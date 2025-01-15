26c1d03e83f1681e364e3c8ad81e94e9c68d21c8
Fabian David <f.duenas@coink.com>
2025-01-15 09:34:57 -0500

    [UPDATE] add cors to socket io

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,7 @@ const subClient = pubClient.duplicate();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
+  cors: { origin: '*' },
   transports: ['websocket'],
   adapter: createAdapter(pubClient, subClient),
 });
